3a32af0f153fdb4998fb9693e7bccf734b70c851
TatiRamirez <tatianramirezarro@gmail.com>
2025-05-09 08:53:14 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bienvenido al curso de Telemática'
+    return 'Bienvenido al curso de Telemática - HOLA'
 
 @app.route('/render')
 def render():
